fe5c3b2e7629eeb601b796773526faaf218a258b
Satoshi Tanaka <tanarky@yahoo.co.jp>
2009-08-15 19:35:36 +0900

    add famfam icons

--- css_splite/get_icons.pl
+++ css_splite/get_icons.pl
@@ -3,9 +3,12 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use Getopt::Long;
+use IO::File;
+
+GetOptions('-css=s', \$::opts{css});
 
 my $output     = "icons.png";
-#my @icons      = ();
 my @icon_names = ();
 my @icon_files = ();
 while(<>){
@@ -14,10 +17,13 @@ while(<>){
     next unless(defined $name && defined $url);
     next unless($url =~ m|/(.*?)(\.gif)$| || $url =~ m|/(.*?)(\.png)$| );
     my $ext = $2;
+    my $file = "$name$ext";
 
-    my $cmd = "/usr/bin/wget $url -O $name$ext";
-    system($cmd);
-    #push @icons, {'name'=>$name, 'file'=>"$name$ext"};
+    if(!-f $file){
+        my $cmd = "/usr/bin/wget $url -O $name$ext";
+        system($cmd);
+    }
+
     push @icon_names, $name;
     push @icon_files, "$name$ext";
 }
@@ -27,3 +33,21 @@ system($convert);
 
 my $optimize = "/usr/bin/optipng $output";
 system($optimize);
+
+if(defined $::opts{css}){
+    gen_css($::opts{css}, @icon_names);
+}
+
+sub gen_css {
+    my ($file, @names) = @_;
+    my $fh = new IO::File "> $file" or die;
+    my $head = "span.icon{overflow:hidden;display:inline-block;height:0;background:url(icons.png);width:16px;padding-top:16px;margin-right:2px;margin-left:2px}";
+    print $fh $head;
+
+    my $i=0;
+    foreach my $n (@names){
+        my $p = ($i) ? $i * 16 * (-1) : 0; $i++;
+        print $fh "span.$n"."{background-position:$p"."px 0}";
+    }
+    $fh->close;
+}
