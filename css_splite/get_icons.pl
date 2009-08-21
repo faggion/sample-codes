b13787cbe21ddb3dad55de3d02b54be8d4f82287
Satoshi Tanaka <tanarky@yahoo.co.jp>
2009-08-22 00:47:04 +0900

    add option --vertical

--- css_splite/get_icons.pl
+++ css_splite/get_icons.pl
@@ -6,9 +6,18 @@ use Data::Dumper;
 use Getopt::Long;
 use IO::File;
 
-GetOptions('-css=s', \$::opts{css});
+GetOptions('-css=s'    => \$::opts{css},
+           '-img=s'    => \$::opts{img},
+           '-vertical' => \$::opts{vertical},
+);
 
-my $output     = "icons.png";
+#my $output     = "./css/icons.png";
+my $output     = $::opts{img};
+#my $output_css = "./css/icons.css";
+my $output_css = $::opts{css};
+
+die if(!defined $output);
+
 my @icon_names = ();
 my @icon_files = ();
 while(<>){
@@ -17,37 +26,43 @@ while(<>){
     next unless(defined $name && defined $url);
     next unless($url =~ m|/(.*?)(\.gif)$| || $url =~ m|/(.*?)(\.png)$| );
     my $ext = $2;
-    my $file = "$name$ext";
+    my $file = "./$name$ext";
 
     if(!-f $file){
-        my $cmd = "/usr/bin/wget $url -O $name$ext";
+        my $cmd = "/usr/bin/wget $url -O $file";
         system($cmd);
     }
 
     push @icon_names, $name;
-    push @icon_files, "$name$ext";
+    push @icon_files, $file;
 }
 
-my $convert = "/usr/bin/convert +append ". join(" ", @icon_files). " $output";
+my $dir = (defined $::opts{vertical}) ? '-' : '+';
+my $convert = "/usr/bin/convert $dir". "append ". join(" ", @icon_files). " $output";
 system($convert);
 
 my $optimize = "/usr/bin/optipng $output";
 system($optimize);
 
-if(defined $::opts{css}){
-    gen_css($::opts{css}, @icon_names);
+if(defined($output_css)){
+    &gen_css((defined $::opts{vertical}), $output_css, @icon_names);
 }
 
 sub gen_css {
-    my ($file, @names) = @_;
+    my ($is_vertical, $file, @names) = @_;
     my $fh = new IO::File "> $file" or die;
-    my $head = "span.icon{overflow:hidden;display:inline-block;height:0;background:url(icons.png) no-repeat;width:16px;padding-top:16px;margin-right:2px;margin-left:2px;vertical-align:top}";
+    my $head = "span.icon{overflow:hidden;display:inline-block;height:0;background:url(icons.png) no-repeat;width:16px;padding-top:16px;margin-right:2px;margin-left:2px;vertical-align:middle}";
     print $fh $head;
 
     my $i=0;
     foreach my $n (@names){
         my $p = ($i) ? $i * 16 * (-1) : 0; $i++;
-        print $fh "span.$n"."{background-position:$p"."px 0}";
+        if($is_vertical){
+            print $fh "span.$n"."{background-position:0 $p"."px}";
+        }
+        else{
+            print $fh "span.$n"."{background-position:$p"."px 0}";
+        }
     }
     $fh->close;
 }
