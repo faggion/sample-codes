ce4598f76db36f7c61b87b6e8630a3dbe2a3da68
Satoshi Tanaka <tanarky@yahoo.co.jp>
2009-11-25 01:09:06 +0900

    use simplejson

--- gae/tanargle/tanargle.py
+++ gae/tanargle/tanargle.py
@@ -1,14 +1,25 @@
+# -*- coding: utf-8 -*-
 from google.appengine.api import users
+from django.utils import simplejson
 
-print 'Content-Type: text/html'
+print 'Content-Type: application/json'
 print ''
 
+back = "/"
+res  = {}
 user = users.get_current_user()
-print 'Hello, Google App Engine! "tanargle"<br>'
 if not user:
-    print '<a href="'+ users.create_login_url("/")+ '">lets login</a>'
+    res["nickname"] = 'ゲストさん'
+    res["login"]    = users.create_login_url(back)
 else:
-    print "Hello, %s!" % user.nickname()
+    res["nickname"] = user.nickname()
+    res["user_id"]  = user.user_id()
+    res["logout"]   = users.create_logout_url(back)
+
+
+print simplejson.dumps(res, ensure_ascii=False)
+
+
 
 
 
