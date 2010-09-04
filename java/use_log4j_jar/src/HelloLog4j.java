77cce87cf9172436a30bf81fbed92563a42ff72c
Satoshi Tanaka <satoshi@satoshi-mac-mini.local>
2010-09-04 17:55:23 +0900

    java use log4 jar

---
+++ java/use_log4j_jar/src/HelloLog4j.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.*;
+
+public class HelloLog4j {
+    public static void main(String[] args) {
+        // インスタンスの生成
+        Logger logger = Logger.getLogger(HelloLog4j.class);
+        PropertyConfigurator.configure("log4j.properties");
+
+        //ログ出力
+        logger.info("This is info.");
+        logger.warn("This is warn.");
+        logger.error("This is error.");
+    }
+}
