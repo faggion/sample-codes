e5e145f1bdd446e9c19a8c37f4d0ec5cb71abad1
Satoshi Tanaka <satoshi@ubuntu.(none)>
2010-09-05 18:10:33 +0900

    2010/09/05 commited

--- c/write/write_load.c
+++ c/write/write_load.c
@@ -9,6 +9,8 @@
 #define __USE_GNU
 #include <fcntl.h>
 
+#define BLOCKSIZE 512
+
 int main(int argc, const char **argv){
     if(argc != 3){
         usage();
@@ -16,16 +18,18 @@ int main(int argc, const char **argv){
     }
 
     int fd, ret, i, size = atoi(argv[1]), loop = atoi(argv[2]);
-    void *buffer;
+    //void *buffer;
+    void *buffer = calloc(1024, sizeof(char));
     char file[] = "./tmp/file";
     char body[size];
     for(i=0;i<size;i++){
         body[i] = 'a';
     }
 
-    buffer = valloc(size);
+    //buffer = valloc(size);
     //posix_memalign(&buffer, BLOCKSIZE, BLOCKSIZE);
-    memcpy(buffer, body, sizeof(body));
+    //memcpy(buffer, body, sizeof(body));
+    memcpy(buffer, body, size);
 
     for(i=0;i<loop;i++){
         fd = open(file, O_CREAT | O_RDWR | O_DIRECT, 0666 );
@@ -38,8 +42,13 @@ int main(int argc, const char **argv){
             close(fd);
             return -1;
         }
+        printf("[%s]\n",(char *)buffer);
+        printf("[%s]\n",body);
+        printf("[%d]\n",size);
+        printf("[%d]\n",sizeof(body));
         close(fd);
     }
+    free(buffer);
     return 0;
 }
 
