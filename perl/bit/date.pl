f2c5771261e3832b0540f6134d6b518683fd9457
Satoshi Tanaka <tanarky@gmail.com>
2011-01-09 02:18:54 +0900

    add bit date function

--- perl/bit/date.pl
+++ perl/bit/date.pl
@@ -4,26 +4,36 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-#my $year = 2010;
-my $year = 4095;
-my $mon  = 16;
-my $day  = 9;
+sub encode_date {
+    my $year = shift @_;
+    my $mon  = shift @_;
+    my $day  = shift @_;
+    return (($year << 9) | ($mon << 5) | $day);
+}
+
+sub decode_date {
+    my $intdate = shift @_;
+    my $day  = ($intdate &     0x1f);
+    my $mon  = ($intdate &    0x1ff) >> 5;
+    my $year = ($intdate & 0x1fffff) >> 9;
+    return ($year, $mon, $day);
+}
+
+my ($year, $mon, $day);
+
+#$year = 4095; # max
+$year = 2011;
+$mon  = 1;
+$day  = 20;
 
 warn sprintf("%04d/%02d/%02d", $year, $mon, $day);
+warn encode_date($year, $mon, $day);
+warn Dumper(decode_date(encode_date($year, $mon, $day)));
 
-warn $year << 9;
-warn $mon  << 5;
-warn $day;
+$year = 2010;
+$mon  = 12;
+$day  = 9;
 
-my $intdate = (($year << 9) | ($mon << 5) | $day);
-
-warn "-------";
-warn $intdate;
-warn "-------";
-
-$day  = ($intdate &     0x1f);      # 0,0000,0000,0000,0001,1111
-$mon  = ($intdate &    0x1ff) >> 5; # 0,0000,0000,0001,1111,1111
-$year = ($intdate & 0x1fffff) >> 9; # 1,1111,1111,1111,1111,1111
-warn $day;
-warn $mon;
-warn $year;
+warn sprintf("%04d/%02d/%02d", $year, $mon, $day);
+warn encode_date($year, $mon, $day);
+warn Dumper(decode_date(encode_date($year, $mon, $day)));
