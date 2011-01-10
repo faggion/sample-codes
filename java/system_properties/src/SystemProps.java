bc04f7e5568a687423545ca89e8cf3b134ed28ba
Satoshi Tanaka <tanarky@gmail.com>
2011-01-10 18:59:21 +0900

    show classpath sample and build.xml.template changed(add includeantruntime=false)

--- java/system_properties/src/SystemProps.java
+++ java/system_properties/src/SystemProps.java
@@ -1,7 +1,11 @@
+import java.util.Properties;
+
 // HelloWorld.java
 public class SystemProps {
     public static void main(String[] args) {
         System.out.println("Hello, Java.");
+        Properties prop = System.getProperties();
+        System.out.println("classpath = " + prop.getProperty("java.class.path", null));
 
         String property_foo = System.getProperty("foo");
 
