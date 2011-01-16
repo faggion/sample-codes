82a62961a530dd75414de6221ae418f4dc7f1e11
Satoshi Tanaka <tanarky@gmail.com>
2011-01-16 11:56:36 +0900

    fix bit/date.pl: add type

--- perl/bit/date.pl
+++ perl/bit/date.pl
@@ -5,10 +5,11 @@ use warnings;
 use Data::Dumper;
 
 sub encode_date {
+    my $type = shift @_;
     my $year = shift @_;
     my $mon  = shift @_;
     my $day  = shift @_;
-    return (($year << 9) | ($mon << 5) | $day);
+    return ($type << 21 | $year << 9 | $mon << 5 | $day);
 }
 
 sub decode_date {
@@ -16,24 +17,30 @@ sub decode_date {
     my $day  = ($intdate &     0x1f);
     my $mon  = ($intdate &    0x1ff) >> 5;
     my $year = ($intdate & 0x1fffff) >> 9;
-    return ($year, $mon, $day);
+    my $type = $intdate >> 21;
+    return ($type, $year, $mon, $day);
 }
 
+my $type = 2;
 my ($year, $mon, $day);
-
 #$year = 4095; # max
 $year = 2011;
 $mon  = 1;
 $day  = 20;
 
 warn sprintf("%04d/%02d/%02d", $year, $mon, $day);
-warn encode_date($year, $mon, $day);
-warn Dumper(decode_date(encode_date($year, $mon, $day)));
+my $e = encode_date($type, $year, $mon, $day);
+warn Dumper(decode_date($e));
+
+warn '-----';
 
 $year = 2010;
 $mon  = 12;
 $day  = 9;
 
 warn sprintf("%04d/%02d/%02d", $year, $mon, $day);
-warn encode_date($year, $mon, $day);
-warn Dumper(decode_date(encode_date($year, $mon, $day)));
+warn encode_date($type, $year, $mon, $day);
+warn Dumper(decode_date(encode_date($type, $year, $mon, $day)));
+
+
+
