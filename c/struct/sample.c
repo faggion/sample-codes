1dde06ef38c66742e3cae2f70238610a34d8f9fb
Satoshi Tanaka <tanarky@gmail.com>
2011-04-04 00:48:49 +0900

    add memcpy, strcpy, malloc

--- c/struct/sample.c
+++ c/struct/sample.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <nshm.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct _test_struct {
-    long key;
+    int key;
     double val;
-    char name[20];
 } test_struct;
 
+typedef struct _str_struct {
+    int id;
+    char name[20];
+} str_struct;
+
+int do_test_struct(test_struct *ts){
+    ts->key = 1;
+    ts->val = ts->key + 10.0;
+    fprintf(stderr, "key[%d], val[%f]\n", ts->key, ts->val);
+    return 0;
+}
+
 int main(void){
-    test_struct ts;
-    ts.key = 1;
-    ts.val = ts.key + 10.0;
-    fprintf(stderr, "key[%d], val[%f]\n", ts.key, ts.val);
-    fprintf(stderr, "size of test_struct[%d]\n", sizeof(test_struct));
+    test_struct *ts;
+    str_struct ss;
+    //char name[20] = "tanarky";
+    char *str;
+
+    ts = (test_struct *)malloc(sizeof(test_struct));
+    do_test_struct(ts);
+    free(ts);
+
+    str = (char *)malloc(sizeof(char) * 20);
+    free(str);
+
+    ss.id = 10;
+    memset(ss.name, 0, sizeof(char) * 20);
+    //memcpy(ss.name, name, 20);
+    strcpy(ss.name, "tanarky");
+
     return 0;
 }
