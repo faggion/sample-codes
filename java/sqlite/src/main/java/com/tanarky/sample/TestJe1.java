a2203f1edfcd1d4b71dcce7f3afc5f4351c26014
Satoshi Tanaka <satoshi.tanaka@gree.net>
2012-05-25 15:57:52 +0900

    added sqlite samples

---
+++ java/sqlite/src/main/java/com/tanarky/sample/TestJe1.java
@@ -0,0 +1,52 @@
+package com.tanarky.sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestJe1 {
+    public static void main(String argv[]) {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:/tmp/sample.sqlite");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while(rs.next()){
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        catch(Exception e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+        finally {
+            try {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e) {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
+        return;
+    }
+}
