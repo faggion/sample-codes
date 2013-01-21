8d0b47cbaca56b8d9afc60d8a459492c03790c10
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-01-21 14:01:24 +0900

    fixed blueprint sample

--- python/flask/blueprint/hello/server.py
+++ python/flask/blueprint/hello/server.py
@@ -4,12 +4,15 @@ from flask import Flask
 from tools.admin   import app as admin
 from tools.manager import app as manager
 from shop.page     import app as shop_page
-from shop.cart     import app as shop_cart
+from shop.cart     import shop_cart
 
 app = Flask(__name__)
+app.secret_key = 'tanarky'
+
 app.register_blueprint(admin)
 app.register_blueprint(manager)
 app.register_blueprint(shop_page)
-app.register_blueprint(shop_cart)
+app.register_blueprint(shop_cart, url_prefix='/cart')
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
--- python/flask/blueprint/hello/shop/cart.py
+++ python/flask/blueprint/hello/shop/cart.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from flask import request, render_template, redirect, url_for, flash
 from flask import Blueprint
 
-app = Blueprint('shop_cart', __name__, url_prefix='/Cart')
+shop_cart = Blueprint('shop_cart', __name__)
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():         
-    return 'Shop Cart Page'
+@shop_cart.route('/', methods=['GET'])
+def index():
+    T = {'name':'hello'}
+    return render_template('shop_cart.html',T=T)
+    
