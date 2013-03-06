836159c07354f23780b0535b1ec3f2fdea003312
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-03-06 18:03:58 +0900

    fixed: dti setup command and python fabric example

--- operation/setup_rvm_on_mac.sh
+++ operation/setup_rvm_on_mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo date
 sudo curl -L get.rvm.io | sudo bash -s stable
 source /etc/profile.d/rvm.sh
-rvm install 1.9.3
+sudo rvm install 1.9.3
---
+++ python/fabric/fabfile.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from fabric.api import run, cd, abort, require, sudo, env, parallel
+from fabric.decorators import runs_once, roles, hosts
+from fabric.contrib.console import confirm
+
+def hello():
+    print('Hello, Tanarky.')
+
+def production():
+    env.roledefs.update({
+            'webservers': ['localhost'],
+            'dbservers': ['localhost:22', 'localhost'],
+            })
+
+def staging():
+    env.roledefs.update({
+            'webservers': ['127.0.0.1'],
+            'dbservers': ['satoshi@127.0.0.1:22', 'localhost'],
+            })
+
+@parallel
+@roles('dbservers')
+def do_release():
+    run("touch /tmp/released_to_webservers")
+
+@hosts('localhost')
+def do_something():
+    run("touch /tmp/fabric")
+    sudo("touch /tmp/fabric_root")
+
