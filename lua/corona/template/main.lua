ab097a922de5f8b63eeb4315dfc1caa8e4912f56
Satoshi Tanaka <tanarky@gmail.com>
2013-04-06 23:39:12 +0900

    showWebPopup sample

--- lua/corona/template/main.lua
+++ lua/corona/template/main.lua
@@ -7,11 +7,17 @@ storyboard.gotoScene( "scene1" )
 
 local tabButtons =
 {
-   { label="First", defaultFile="icon1.png", overFile="icon1.png",
-     width=38, height=38, size=12, selected=true,
+   { label="Home", defaultFile="1.png", overFile="1.png",
+     width=32, height=32, size=12, selected=true,
+     labelColor = { default={255, 255, 255, 80}, over={255, 255, 255} },
+     onPress=function() storyboard.gotoScene( "scene2" ); end },
+
+   { label="Browse", defaultFile="34.png", overFile="34.png",
+     width=32, height=32, size=12, selected=false,
      labelColor = { default={255, 255, 255, 80}, over={255, 255, 255} },
      onPress=function() storyboard.gotoScene( "scene1" ); end },
-   { label="Second", defaultFile="icon2.png", overFile="icon2.png",
+
+   { label="Config", defaultFile="53.png", overFile="53.png",
      width=38, height=38, size=12, selected=false,
      labelColor = { default={255, 255, 255, 80}, over={255, 255, 255} },
      onPress=function() storyboard.gotoScene( "scene2" ); end },
--- lua/corona/template/scene1.lua
+++ lua/corona/template/scene1.lua
@@ -1,8 +1,35 @@
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
+local webView = nil
+
 function scene:createScene( event )
    local group = self.view
+   --webView = native.newWebView( 0, 0, 320, 480 )
+   --webView:request( "http://www.coronalabs.com/" )
+   --webView:request( "local.html" )
+   --group:insert(webView)
    print("hello scene1")
+
+   local function listener( event )
+      local shouldLoad = true
+      local url = event.url
+      if 1 == string.find( url, "corona:close" ) then
+         shouldLoad = false
+      end
+      if event.errorCode then
+         print( "Error: " .. tostring( event.errorMessage ) )
+         shouldLoad = false
+      end
+      return shouldLoad
+   end
+   local options = {
+      hasBackground=true,
+      baseUrl=system.ResourceDirectory,
+      urlRequest=listener
+   }
+   native.showWebPopup(0, 0, 320, 436, "local.html", options)
+   --native.showWebPopup(0, 0, 320, 436, "http://www.yahoo.co.jp/")
+   
 end
 
 function scene:enterScene( event )
@@ -18,6 +45,8 @@ end
 function scene:destroyScene( event )
    local group = self.view
    print("destroy scene1")
+   webView:removeSelf()
+   webView = nil
 end
 
 scene:addEventListener( "createScene", scene )
