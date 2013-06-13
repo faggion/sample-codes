36eae7a1ee52cb740a86b4be22f581c56e2e96c3
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-06-13 18:26:19 +0900

    fixed

--- c/lockfree/test.c
+++ c/lockfree/test.c
@@ -2,6 +2,9 @@
 
 int main(void){
     int a = 10;
+    // __sync_bool_compare_and_swap(A,B,C)
+    // __sync_add_and_fetch(A,B)
+    // __sync_sub_and_fetch(A,B)
     printf("a 1 = %d\n", a);
     printf("a 2 = %d\n", __sync_add_and_fetch(&a, 10));
     printf("a 3 = %d\n", a);
