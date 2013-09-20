3b97dc4cd6d4cec3351cb795e842da54c38b260d
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-09-20 02:30:22 +0000

    fixed

--- c/sptree/tools/gen/sptree_gen.cc
+++ c/sptree/tools/gen/sptree_gen.cc
@@ -34,13 +34,96 @@ std::vector<std::string> split(const std::string &str, char delim){
 
 static bool get_sptree_map(std::map<int64_t, mapv_t> &sp_map){
     std::string line;
+    std::vector<std::string> datas;
     char *index[5];
     mapv_t val;
-    //vector data;
 
     while(std::getline(std::cin, line)){
-        std::cout << line << std::endl;
+        if(!line.compare(0, 1, "#")){
+            //std::cerr << "This is comment line: " << line << std::endl;
+            continue;
+        }
+
+        datas = split(line, '\t');
+        if(datas.size() != 3){
+            continue;
+        }
+
+        val.node.id      = strtol(datas[0].c_str(), NULL, 10);
+        val.parent_id    = strtol(datas[1].c_str(), NULL, 10);
+        val.node.site_id = strtol(datas[2].c_str(), NULL, 10);
+
+        val.node.depth        = -1;
+        val.node.parent_index = -1;
+        val.index             = -1;
+        sp_map[val.node.id]   = val;
     }
+    return(true);
+}
+
+static void fix_sptree_map(std::map<int64_t,mapv_t> &sp_map){
+    std::map<int64_t,mapv_t>::iterator it;
+    std::map<int64_t,mapv_t>::iterator parent_it;
+    int ix;
+
+    for(ix = 0, it = sp_map.begin(); it != sp_map.end(); ++it, ix++){
+        it->second.index = ix;
+    }
+
+    for(it = sp_map.begin(); it != sp_map.end(); ++it){
+        parent_it = sp_map.find(it->second.parent_id);
+
+        if(parent_it != sp_map.end()){
+            it->second.node.parent_index = parent_it->second.index;
+        }
+        else{
+            it->second.node.parent_index = -1;
+        }
+    }
+
+    for(it = sp_map.begin(); it != sp_map.end(); ++it){
+        int64_t parent_id = it->second.parent_id;
+
+        for(ix = 0; 0 <= parent_id; ix++){
+            parent_it = sp_map.find(parent_id);
+
+            if(parent_it != sp_map.end()){
+                parent_id = parent_it->second.parent_id;
+            }
+            else{
+                parent_id = -1;
+            }
+        }
+
+        it->second.node.depth = ix;
+    }
+    return;
+}
+
+static bool write_sptree(int fd,const std::map<int64_t,mapv_t> &sp_map)
+{
+    sptree_header_t header;
+    std::map<int64_t,mapv_t>::const_iterator it;
+
+    memset(&header,'\0',sizeof(header));
+    strncpy(header.magic, SPTREE_MAGIC, sizeof(header.magic));
+
+    header.version = SPTREE_VERSION;
+    header.replaced = 0;
+    header.ctime = time(NULL);
+
+    if(write(fd,&header,sizeof(header)) < 0){
+        fprintf(stderr,"ERROR: write header: %s\n", strerror(errno));
+        return(false);
+    }
+
+    for(it = sp_map.begin(); it != sp_map.end(); ++it){
+        if(write(fd, &(it->second.node), sizeof(it->second.node)) < 0){
+            fprintf(stderr,"ERROR: write: %s\n",strerror(errno));
+            return(false);
+        }
+    }
+
     return(true);
 }
 
@@ -51,10 +134,12 @@ static bool gen_sptree(int fd){
         return(false);
     }
 
-    //fix_sptree_map(sp_map);
-    //if(!write_sptree(fd, sp_map)){
-    //    return(false);
-    //}
+    fix_sptree_map(sp_map);
+
+    if(!write_sptree(fd, sp_map)){
+        return(false);
+    }
+
     return(true);
 }
 
