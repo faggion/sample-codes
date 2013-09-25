3edb3fda6df29b679cb1c08e2c712ef51d5441b5
Satoshi Tanaka <tanarky@gmail.com>
2013-09-25 23:41:02 +0900

    fixed

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 //	"github.com/tanarky/string"
-	"github.com/tanarky/rand"
+//	"github.com/tanarky/rand"
 )
 
 func main() {
 	//fmt.Println(string.Reverse(fmt.Sprintf("Hello, world; こんにちは、世界; %d", rand.Random())))
+        fmt.Println("Hello, world; こんにちは、世界")
 
-	for i:=0; i<3; i++ {
-		rand.Seed(1)
-		fmt.Println(fmt.Sprintf("rand: %d", rand.Random()))
-	}
+        //for i:=0; i<3; i++ {
+	//	rand.Seed(1)
+	//	fmt.Println(fmt.Sprintf("rand: %d", rand.Random()))
+	//}
 }
