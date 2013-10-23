84ee4aeb40364729563c4df69a1d279ded75bd97
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-10-23 11:29:37 +0000

    fixed

--- c/zeromq/pub_sub/client.c
+++ c/zeromq/pub_sub/client.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include "zhelpers.h"
+#include <string.h>
+
+#include <zmq.h>
 
 int main (void)
 {
+    int rc;
+    size_t size;
+    char *buf = NULL;
+
     void *ctx = zmq_ctx_new();
     void *sub = zmq_socket(ctx, ZMQ_SUB);
     void *dealer = zmq_socket(ctx, ZMQ_DEALER);
@@ -15,11 +21,19 @@ int main (void)
 
     while (1) {
         fprintf(stderr, "receiving...\n");
-        char *cmd = s_recv(sub);
-        fprintf(stderr, "cmd: %s\n", cmd);
 
-        s_send(dealer, cmd);
-        free (cmd);
+        zmq_msg_t msg;
+        rc = zmq_msg_init(&msg);
+        rc = zmq_msg_recv (&msg, sub, 0);
+        fprintf(stderr, "msg size: %ld\n", zmq_msg_size(&msg));
+
+        size = zmq_msg_size(&msg) + 1;
+        buf = (char *)malloc(size);
+        memset(buf, 0, size);
+        memcpy(buf, zmq_msg_data(&msg), size - 1);
+        fprintf(stderr, "msg data: %s\n", buf);
+
+        zmq_msg_close(&msg);
     }
 
     zmq_close(sub);
