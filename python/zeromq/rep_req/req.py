0a9a6f8ec66ecd28a16cd992ba12b6716d7d634f
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-11-05 11:18:19 +0900

    fixed

--- python/zeromq/rep_req/req.py
+++ python/zeromq/rep_req/req.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import zmq
+import zmq, sys
 
 ctx = zmq.Context()
 sock = ctx.socket(zmq.REQ)
@@ -8,7 +8,7 @@ sock.connect('tcp://127.0.0.1:5555')
 
 # REQ側はレスポンスを受け取るまで新たなリクエストを出来ないし
 # REP側はレスポンスを返すまで他のリクエストを受け取れない
+sock.send(sys.argv[1])
 sock.send('hello')
-#sock.send('hello')
 print sock.recv()
 
