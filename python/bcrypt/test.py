5c1a5a9e289b65b4f03ffda5abcc70f516086af5
Satoshi Tanaka <satoshi.tanaka@gree.net>
2014-03-20 13:53:20 +0900

    fixed

--- python/bcrypt/test.py
+++ python/bcrypt/test.py
@@ -5,7 +5,8 @@ from Crypto.Cipher import Blowfish
 def PKCS5Padding(string):
     byteNum = len(string)
     packingLength = 8 - byteNum % 8
-    appendage = chr(packingLength) * packingLength
+    #appendage = chr(packingLength) * packingLength
+    appendage = '\x00' * packingLength
     logging.debug("padding char is %s" % ord(chr(packingLength)))
     return string + appendage
 
@@ -15,15 +16,13 @@ def PKCS5Unpadding(string):
     logging.debug("last char is %d" % c)
     return string[0:stringlen-c]
 
-def PandoraEncrypt(string):
-    key = b'6#26FRL$ZWD'
+def PandoraEncrypt(key, string):
     c1  = Blowfish.new(key, Blowfish.MODE_ECB)
     packedString = PKCS5Padding(string)
     logging.debug("packedString len = %d" % len(packedString))
     return c1.encrypt(packedString)
 
-def PandoraDecrypt(string):
-    key = b'6#26FRL$ZWD'
+def PandoraDecrypt(key, string):
     c1  = Blowfish.new(key, Blowfish.MODE_ECB)
     unpacked_str = c1.decrypt(string)
     return unpacked_str
@@ -31,14 +30,17 @@ def PandoraDecrypt(string):
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
 
+    key = b'The quick brown fox jumps over the lazy dog.'
+
     ## 12 bytes
     #s = 'testingshoge'
     # 8 bytes
-    s = b'testings' 
+    #s = b'testings' 
+    s = b"This is not a pipe."
     logging.debug(len(s))
-    c = PandoraEncrypt(s)
+    c = PandoraEncrypt(key, s)
 
     encoded_str = base64.b64encode(c)
     logging.debug(encoded_str)
-    e = PandoraDecrypt(base64.b64decode(encoded_str))
+    e = PandoraDecrypt(key, base64.b64decode(encoded_str))
     logging.debug("'%s'" % PKCS5Unpadding(e))
