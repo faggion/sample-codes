78c54fc39af98e7beb7fd378145efc6b4e739ac1
Satoshi Tanaka <tanarky@gmail.com>
2014-07-22 00:55:58 +0900

    fixed

--- lua/corona/custom_fonts/splash.lua
+++ lua/corona/custom_fonts/splash.lua
@@ -1,25 +1,17 @@
 -- TODO: animation
 
-local storyboard = require( "storyboard" )
+local storyboard = require("storyboard")
+local page = require("page")
 local scene = storyboard.newScene()
+local DISPLAY_WIDTH  = display.contentWidth
+local DISPLAY_HEIGHT = display.contentHeight
 
 function scene:createScene( event )
 end
 
 function scene:enterScene( event )
    local group = self.view
-   print("enter scene1")
-
-   local DISPLAY_WIDTH  = display.contentWidth
-   local DISPLAY_HEIGHT = display.contentHeight
-   local TITLE = {
-      text="日本語も表示される",
-      font={
-         size=30,
-         --family=native.systemFont,
-         family="PixelMplus10-Regular",
-      }
-   }
+   print("enter splash")
 
    -- display scale values
    local bg = display.newRect(group,DISPLAY_WIDTH/2,
@@ -27,32 +19,19 @@ function scene:enterScene( event )
                               DISPLAY_WIDTH,
                               DISPLAY_HEIGHT)
    bg:setFillColor(0.1,0.1,0.1)
-   --group:insert(bg)
 
-   local title = display.newText("", 0, 0,
-                                 TITLE.font.family,
-                                 TITLE.font.size)
-   --title.isVisible = false
-   title.text = TITLE.text
-   -- object中心の位置がx,y (左上ではない)
-   title.x = DISPLAY_WIDTH/2
-   title.y = DISPLAY_HEIGHT/2 + 10
-   title:setFillColor(0.8,0.8,0.8)
-
-   --title.isVisible = true
-   --group:insert(title)
-
-   --timer.performWithDelay(500, function() storyboard.gotoScene("requests"); end )
+   local pg = page:new("scene_1",
+                       {"こんにちは", "ようこそ"})
 end
 
 function scene:exitScene( event )
    local group = self.view
-   print("exit scene1")
+   print("exit splash")
 end
 
 function scene:destroyScene( event )
    local group = self.view
-   print("destroy scene1")
+   print("destroy splash")
 end
 
 scene:addEventListener( "createScene", scene )
