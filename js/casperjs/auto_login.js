ad7ee8efc9b047246ae1e4ab87cc046d2aee9183
Satoshi Tanaka <tanarky@gmail.com>
2014-08-31 00:06:40 +0900

    fixed

--- js/casperjs/auto_login.js
+++ js/casperjs/auto_login.js
@@ -2,16 +2,21 @@ var links = [];
 var casper = require('casper').create();
 
 casper.start('http://localhost:5000/', function() {
-    this.fill('form[action="/update"]',
-              { username: 'valid_user',
-                password: 'invalid_password' },
-              false);
-    this.evaluate(function() {
-        //document.querySelector('#submit').click();
-        //document.querySelector('input[type="submit"]').click();
-        document.querySelector('form[action="/update"] input[type="submit"]').click();
-    }); 
-    this.capture('mail0.png');
+    var needLogin = this.evaluate(function(){
+        return document.querySelector('#name') ? true : false;
+    });
+    console.log(needLogin);
+
+    if(needLogin){
+        this.fill('form[action="/login"]',
+                  { name: 'valid_user',
+                    password: 'invalid_password' },
+                  false);
+
+        this.evaluate(function() {
+            document.querySelector('form[action="/login"] input[type="submit"]').click();
+        });
+    }
 });
 
 casper.then(function() {
--- js/casperjs/server.py
+++ js/casperjs/server.py
@@ -1,17 +1,32 @@
 # coding: utf-8
 import os, sys, logging
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/', methods=['GET'])
 def index():
+    name = request.cookies.get('name')
+    logging.debug(request.cookies)
+    logging.debug(request.cookies.get('name'))
+    if name:
+        T = {'name':name}
+        return render_template('index.html', T=T)
     return render_template('form.html')
 
-@app.route('/update', methods=['POST'])
-def update():
-    logging.debug(request.form)
-    return redirect(url_for('index'))
+@app.route('/login', methods=['POST'])
+def login():
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('name', request.form.get('name',''))
+    return response
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logging.debug(request.headers)
+    response = app.make_response(redirect(url_for('index')))
+    response.set_cookie('name', '')
+    return response
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
